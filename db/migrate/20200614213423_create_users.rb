597073d2e70dd396678f3532ce6523d26ff562c9
Roberto-Livi <robertomlivi@outlook.com>
2020-06-14 17:37:16 -0400

    added users table

---
+++ db/migrate/20200614213423_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password_digest
+      t.string :title
+      t.string :birth_date
+      t.string :education
+      t.string :skills
+
+      t.timestamps null: false
+    end
+  end
+end
