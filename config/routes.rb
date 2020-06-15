909a886c65855c0313046f67cc94f616db683318
Roberto-Livi <robertomlivi@outlook.com>
2020-06-14 23:07:04 -0400

    organized routes in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+
+  # Home Page
   root "welcome#home"
+
+  #Users
   resources :users
+  get "/logout" => "users#destroy"
+
+  # Posts
   resources :posts
-  get "/logout", to: "users#destroy"
+
 end
