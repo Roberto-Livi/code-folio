492a72e314609cf416dd76ed442b7475ebf87f63
Roberto-Livi <robertomlivi@outlook.com>
2020-06-14 23:54:10 -0400

    postscontroller can now create a post and show it on the users page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,8 @@ class PostsController < ApplicationController
     def create
         if !params[:post][:content].empty?
             post = Post.create(:content => params[:post][:content])
+
+            redirect_to user_path(current_user)
         else
             flash[:empty_post] = "Post can't be empty"
             redirect_to user_path(current_user)
