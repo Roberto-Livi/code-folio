af8477363aeb5ed1c95c1440b927180688aeb63d
Roberto-Livi <robertomlivi@outlook.com>
2020-06-15 00:08:15 -0400

    fixed sessionscontroller bugs and can now offically sign in a user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,11 +3,13 @@ class PostsController < ApplicationController
     def create
         if !params[:post][:content].empty?
             post = Post.create(:content => params[:post][:content])
+            current_user.posts << post
 
             redirect_to user_path(current_user)
         else
             flash[:empty_post] = "Post can't be empty"
             redirect_to user_path(current_user)
+        end
     end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,11 @@ class SessionsController < ApplicationController
     end
 
     def create
-        user = User.find_by(name: params[:user][:name])
-        if user && user.authenticate(params[:user][:password])
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
             session[:user_id] = user.id
 
-            redirect_to new_welcome_path
+            redirect_to user_path(user)
         else
             redirect_to new_session_path
         end
