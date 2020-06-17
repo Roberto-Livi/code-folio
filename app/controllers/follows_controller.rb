fe1074caf49bee0ec0f367c8f863feff54b42731
Roberto-Livi <robertomlivi@outlook.com>
2020-06-17 11:35:42 -0400

    able to follow and unfollow a user with a click of a button

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,9 +1,20 @@
 class FollowsController < ApplicationController
+    before_action :login_required, :find_user
 
     def create
+        current_user.follow(@user)
+        redirect_to user_path(@user)
     end
 
     def destroy
+        current_user.unfollow(@user)
+        redirect_to user_path(@user)
+    end
+
+    private
+
+    def find_user
+        @user = User.find(params[:user_id])
     end
 
 end
