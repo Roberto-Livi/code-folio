7f925ceccaedfe9c1472dce381060daf8e7fffee
Roberto-Livi <robertomlivi@outlook.com>
2020-06-17 08:59:01 -0400

    created follows model and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_14_214612) do
+ActiveRecord::Schema.define(version: 2020_06_17_125438) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
@@ -18,6 +18,16 @@ ActiveRecord::Schema.define(version: 2020_06_14_214612) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["followed_id"], name: "index_follows_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_follows_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_follows_on_follower_id"
   end
 
   create_table "posts", force: :cascade do |t|
