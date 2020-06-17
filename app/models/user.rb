3ab6e677ff07c2681d494b96982b1479ad0e87c1
Roberto-Livi <robertomlivi@outlook.com>
2020-06-17 09:12:20 -0400

    added associations for follow and users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,26 @@ class User < ActiveRecord::Base
     validates :username, uniqueness: true
     validates :password, length: { in: 5..30 }
 
+    has_many :active_follows, class_name: "Follow", foreign_key: "follower_id", dependent: :destroy
+
+    has_many :passive_follows, class_name: "Follow", foreign_key: "followed_id", dependent: :destroy
+
+    has_many :following, through: :active_follows, source: :followed
+
+    has_many :followers, through: :passive_follows, source: :follower
+
+    def follow(user)
+        active_follows.create(followed_id: user.id)
+    end
+
+    def unfollow(user)
+        active_follows.find_by(followed_id: user.id).destroy
+    end
+
+    def following(user)
+        following.include?(user)
+    end
+
     def self.search(search)
         if search
             @user = User.find_by(:username => search)
