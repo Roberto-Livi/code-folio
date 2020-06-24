c1dc03691a2274adfdf65f8c8e435e911679dd99
Roberto-Livi <robertomlivi@outlook.com>
2020-06-24 18:46:58 -0400

    fixed users post show page where the user can't access the url by changing the users id

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,20 +2,28 @@ class PostsController < ApplicationController
     before_action :find_post, only: [:edit, :update, :show, :destroy]
 
     def create
-        if !params[:post][:content].empty?
+        if !params[:post][:content].empty? && !params[:post][:title].empty?
             post = Post.create(post_params)
             current_user.posts << post
 
             redirect_to user_path(current_user)
         else
-            flash[:empty_post] = "Post can't be empty"
-            redirect_to user_path(current_user)
+            flash[:empty_post] = "Post fields can't be empty"
+            redirect_to users_path(current_user)
         end
     end
 
     def show
-        @user = User.find(params[:user_id])
-        @comment = Comment.new
+        if params[:user_id]
+            @user = User.find(params[:user_id])
+            @post = @user.posts.find_by(id: params[:id])
+            @comment = Comment.new
+            if @post.nil?
+                redirect_to users_path(current_user)
+            end
+        else
+            @post = Post.find(params[:id])
+        end
     end
 
     def edit
